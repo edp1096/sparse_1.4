e0b6a40f5f28b5fcfceb534972388ace4a97bb56
edp1096 <edp1096@naver.com>
2025-01-02 15:53:04 +0900

    remove not necessary

--- src/spFactor.c
+++ src/spFactor.c
@@ -315,7 +315,6 @@ spError spFactor(spMatrix eMatrix) {
     ASSERT_IS_NOT_FACTORED(Matrix);
 
     if (Matrix->NeedsOrdering) {
-        printf("!!!!");
         return spOrderAndFactor(eMatrix, (RealVector)NULL, 0.0, 0.0, DIAG_PIVOTING_AS_DEFAULT);
     }
     if (NOT Matrix->Partitioned) {
@@ -323,7 +322,6 @@ spError spFactor(spMatrix eMatrix) {
     }
 #if spCOMPLEX
     if (Matrix->Complex) {
-        printf("@@111");
         return FactorComplexMatrix(Matrix);
     }
 #endif
